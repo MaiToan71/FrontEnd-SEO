c86a115bda808e3b1bc01d14116b1742578396a8
tek4tv <69296370+tek4tv@users.noreply.github.com>
2024-02-20 21:27:59 +0700

    init

--- src/lib/utils/constant.ts
+++ src/lib/utils/constant.ts
@@ -1,3 +1,3 @@
-export const SERVER_BASE_URL = "https://localhost:44302";
+export const SERVER_BASE_URL = "https://localhost:7066";
 export const DEFAULT_LIMIT = 20;
 export const FRONTEND_URL = "http://localhost:3000";
